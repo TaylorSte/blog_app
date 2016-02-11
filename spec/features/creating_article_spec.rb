a3ddfba8a25b3ad16652d16cf8de9b2c4789c8d6
Ste Taylor <ste.taylor@acom-soft.com>
2016-02-11 05:22:42 +0000

    Add Validations to Article

--- spec/features/creating_article_spec.rb
+++ spec/features/creating_article_spec.rb
@@ -13,4 +13,20 @@ RSpec.feature "Creating Articles" do
 
   end
 
+  scenario "A user fails to create a new article" do
+
+    visit "/" #goto root      #capy
+    click_link "New Article"    #capy
+    fill_in "Title", with:""  #capy
+    fill_in "Body", with: ""   #capy
+
+    click_button "Create Article" #capy
+
+    expect(page).to have_content("Article has not been created")
+    expect(page).to have_content("Title can't be blank")
+    expect(page).to have_content("Body can't be blank")
+
+
+
+    end
 end
